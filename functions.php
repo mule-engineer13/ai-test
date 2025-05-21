dcf7a6b0515930a311b9f0d06837d25b9bbd8b76
mule-engineer13 <112328461+mule-engineer13@users.noreply.github.com>
2025-05-21 20:17:42 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,15 +4,15 @@ function checkEmail(string $email): string
 		$error = null;
     // フォーマットチェック
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       $error = メールのフォーマットが誤っています。
-    }
+    	$error = $content."を確認してください";
+		}
 
     // ドメイン部分を抽出
     [$localPart, $domain] = explode('@', $email);
 
     // DNSチェック（MXレコードがあるか確認）
     if (!checkdnsrr($domain, 'MX')) {
-        $error = "メールのフォーマットが誤っています。"
+      $error = $content."を確認してください";
     }
 
     return $email;
